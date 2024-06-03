31cae8660fb526430dead5c7193bc38981117a31
Victor <josvictoronofre@gmail.com>
2024-06-03 13:39:18 -0300

    finalizando as classes

---
+++ src/NavegadorInternet.java
@@ -0,0 +1,14 @@
+public class NavegadorInternet extends Iphone {
+
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo pagina " + url);
+    }
+
+    public void adicionarNovaAba() {
+        System.out.println("Abrindo nova aba");
+    }
+
+    public void atualizarPagina() {
+        System.out.println("Atualizando pagina");
+    }
+}
